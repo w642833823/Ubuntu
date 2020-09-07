c845c3a0155d15e3b940acb99ceb494c484a277b
root <root@localhost>
2020-09-07 23:51:38 +0800

    歌词

---
+++ Unicode与UTF-8互相转换.cpp
@@ -0,0 +1,27 @@
+//Unicode转UTF-8
+
+std::string  UnicodeToUTF8(const std::wstring& wstr)
+{
+	int ansiiLen = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, nullptr, 0, nullptr, nullptr);
+	char* pAssii = (char*)malloc(sizeof(char) * ansiiLen);
+	WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, pAssii, ansiiLen, nullptr, nullptr);
+	std::string ret_str = pAssii;
+	free(pAssii);
+	return ret_str;
+}
+
+
+//UTF-8转Unicode
+char* UTF8ToUnicode(char* szU8)
+{
+	int wcsLen = ::MultiByteToWideChar(CP_UTF8, NULL, szU8, strlen(szU8), NULL, 0);
+	wchar_t* wszString = new wchar_t[wcsLen + 1];
+	::MultiByteToWideChar(CP_UTF8, NULL, szU8, strlen(szU8), wszString, wcsLen);
+	wszString[wcsLen] = '\0';
+	char* m_char;
+	int len = WideCharToMultiByte(CP_ACP, 0, wszString, wcslen(wszString), NULL, 0, NULL, NULL);
+	m_char = new char[len + 1];
+	WideCharToMultiByte(CP_ACP, 0, wszString, wcslen(wszString), m_char, len, NULL, NULL);
+	m_char[len] = '\0';
+	return m_char;
+}
